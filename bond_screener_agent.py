cd04f1206eb36fc5dccc9ea5d7c6b19888a484d1
SRINJOY59 <srinjoydas566@gmail.com>
2025-03-08 17:23:27 +0530

    bond finder agent done

--- bond_screener_agent.py
+++ bond_screener_agent.py
@@ -13,8 +13,8 @@ from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 
-class CompanyFinancialAnalysisAgent:
-    def __init__(self, csv_path, model_name="llama3-70b-8192", embedding_model="sentence-transformers/all-MiniLM-L6-v2"):
+class BondScreenergent:
+    def __init__(self, csv_path = "company_insights_202503011114.csv", model_name="llama3-70b-8192", embedding_model="sentence-transformers/all-MiniLM-L6-v2"):
         load_dotenv()
         self.csv_path = csv_path
         self.embedding_model = embedding_model
@@ -62,6 +62,6 @@ class CompanyFinancialAnalysisAgent:
 
 
 if __name__ == "__main__":
-    agent = CompanyFinancialAnalysisAgent("company_insights_202503011114.csv")
+    agent = BondScreenergent()
     result = agent.query("Give an overview of the financial analysis of Navi-finserv Limited.")
     print(result)
